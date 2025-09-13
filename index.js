f8a9617a8604899c5e37acdd5671870de7c7f028
Allan Ratemo <allanisratemo@gmail.com>
2025-09-13 13:01:52 +0300

    Used array methods to manipulate the cats array as per the requirements

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution here!
+
+// Cats array
+let cats = ["Milo", "Otis", "Garfield"]
+
+// Appends a cat to the beginning of the cats array
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+// Prepends a cat to the beginning of the cats array
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+// destructivelyPrependCat("Bob");
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+//  Removes the first cat from the cats array
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+// Appends a cat to the cats array and returns a new array, leaving the cats array unchanged
+function appendCat(name) {
+    return [...cats, name];
+}
+
+// Prepends a cat to the cats array and returns a new array, leaving the cats array unchanged
+function prependCat(name) {
+    return [name, ...cats];
+}
+
+// Removes the last cat in the cats array and returns a new array, leaving the cats array unchanged
+function removeLastCat() {
+    return cats.slice(0,-1);
+}
+
+// Removes the first cat from the cats array and returns a new array, leaving the cats array unchanged
+function removeFirstCat() {
+    return cats.slice(1);
+}
+
+console.log(cats);
+
